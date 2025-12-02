2f2f4169be8f8e079a0721753513e58b9867188b
Yanlin-Duanmu <duanmuyanlin@outlook.com>
2025-12-02 18:29:01 +0800

    chore(AiDemoViewModel): remove unused import statement

--- app/src/main/java/com/noteability/mynote/ui/aiDemo/AiScreen.kt
+++ app/src/main/java/com/noteability/mynote/ui/aiDemo/AiScreen.kt
@@ -11,7 +11,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.noteability.mynote.viewmodel.AiDemoViewModel
 
 @OptIn(ExperimentalLayoutApi::class, ExperimentalMaterial3Api::class) // requires opt-in
 @Composable
